4be5b1c2c378ecf32d4f98f4d4931ca5d3194eda
MARTA <martariosrguez@hotmail.com>
2022-08-26 13:06:42 +0200

    Tweets table/ model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_26_105419) do
+ActiveRecord::Schema.define(version: 2022_08_26_110259) do
 
   create_table "sessions", force: :cascade do |t|
     t.string "token"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "message"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
