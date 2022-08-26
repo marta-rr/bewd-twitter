9b75073c869f3993e6986fdbad761cc45214233d
MARTA <martariosrguez@hotmail.com>
2022-08-26 13:01:56 +0200

    Sessions table/ model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_26_103223) do
+ActiveRecord::Schema.define(version: 2022_08_26_105419) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "token"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
