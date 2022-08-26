54f49a605b4c83d05ea91c88ecc6d4e51ff6b0a0
MARTA <martariosrguez@hotmail.com>
2022-08-26 12:46:32 +0200

    User table/ model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
 
+    validates :username, presence: true, length: { minimum: 3, maximum: 64 }
+    validates :password, presence: true, length: { minimum: 8, maximum: 64 }
+    validates :email, presence: true
+    validates_uniqueness_of :username, :email
+
+    has_many :sessions
+    has_many :tweets
+
 end
