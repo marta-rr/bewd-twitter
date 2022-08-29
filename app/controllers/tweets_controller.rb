2ecd8d215671e6b4e6d5cb38afeb147da2065e13
MARTA <martariosrguez@hotmail.com>
2022-08-29 11:51:07 +0200

    Post /tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,3 +1,30 @@
 class TweetsController < ApplicationController
 
+    def create
+        token = cookies.signed[:twitter_session_token]
+        session = Session.find_by(token: token)
+
+        if session
+            user = session.user
+            @tweet = user.tweets.new(tweet_params)
+
+            if @tweet.save
+                render 'tweets/create'
+            else
+                render json: {
+                    success: false
+                }
+            end
+        else
+            render json: {
+                success:  false
+            }
+        end
+    end
+
+    private
+    def tweet_params
+        params.require(:tweet).permit(:message)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
 
   # TWEETS
+  post 'tweets' => 'tweets#create'
 
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
